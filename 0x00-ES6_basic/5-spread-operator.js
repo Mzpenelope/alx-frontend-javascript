786b03be97afecd42b6d3fca6b7c4dda05049db7
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2024-02-06 01:14:03 +0000

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
-export default function concatArrays(...args)
-{
-  return args.flat();
+export default function concatArrays(array1, array2, string){
+  return [...array1, ...array2, ...string].flat();
 }
