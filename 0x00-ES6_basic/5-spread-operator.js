382a1165859448d5fef0b185b0485081276c4c9f
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2024-02-06 01:09:59 +0000

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,10 +1,4 @@
-export default function concatArrays(...args) {
-  return args.reduce((result, current) => {
-    if (Array.isArray(current)) {
-      return [...result, ...current];
-    } else if (typeof current === 'string') {
-      return [...result, ...current.split('')];
-    }
-    return result;
-  }, []);
+export default function concatArrays(...args)
+{
+  return args.flat();
 }
